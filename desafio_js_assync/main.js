a262c2a2470cf6cbc9b26fd5988e60131cc70067
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-06-16 15:30:44 -0300

    atividade de js assíncrono, usando api do Chuck Norris

---
+++ desafio_js_assync/main.js
@@ -0,0 +1,22 @@
+const BASE_URL = 'https://api.chucknorris.io/jokes/random'
+
+const botao = document.getElementById('contar-piada')
+
+
+const getPiada = async () =>{
+   try{
+       const data = await fetch(BASE_URL)
+       const json = await data.json()
+       return json.value
+   }catch(e){
+       console.log(e.message)
+   }
+}
+
+const carregaPiada = async ()=>{
+    const piadaCarregada = document.getElementById('piada')
+    piadaCarregada.innerHTML = await getPiada();
+}
+
+botao.addEventListener('click', carregaPiada)
+carregaPiada()
